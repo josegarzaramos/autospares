53c27eac2adbe116b71010c1692f63b81c49ae1e
Jose Luis Garza <josegarzaramos@gmail.com>
2021-06-02 20:50:14 -0500

    Added Navbar and Sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,12 @@ import { BiCar } from 'react-icons/bi';
 
 const Navbar = () => {
   return (
-    <div>
-      <BiCar />
-      <h1>Logo</h1>
+    <div className="p-4 border-b border-gray-200">
+      <BiCar
+        size="1.5em"
+        className="inline-block mr-2 fill-current text-blue-600"
+      />
+      <h1 className="inline-block font-bold align-middle">Logo</h1>
     </div>
   );
 };
