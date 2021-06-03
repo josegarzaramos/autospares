54b83e01eb002b431d3dcc787a194a857b3e4c36
Jose Luis Garza <josegarzaramos@gmail.com>
2021-06-03 00:48:11 -0500

    Added links to navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,16 @@
 const Sidebar = () => {
   return (
-    <ul>
-      <li>Refacciones</li>
-      <li>Interiores</li>
-      <li>Exteriores</li>
-      <li>Remolque</li>
-      <li>Herramientas</li>
-      <li>Químicos, Aceites y Productos de Lavado</li>
-      <li>Alto Desempeño</li>
-    </ul>
+    <div id="sidebar-links">
+      <ul className="p-4 w-4/5">
+        <li className="px-1 py-2">Refacciones</li>
+        <li className="px-1 py-2">Interiores</li>
+        <li className="px-1 py-2">Exteriores</li>
+        <li className="px-1 py-2">Remolque</li>
+        <li className="px-1 py-2">Herramientas</li>
+        <li className="px-1 py-2">Químicos, Aceites y Productos de Lavado</li>
+        <li className="px-1 py-2">Alto Desempeño</li>
+      </ul>
+    </div>
   );
 };
 
