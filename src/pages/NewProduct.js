62d614fbbd124369bd3d7f3064334707e95a8e79
Jose Luis Garza <josegarzaramos@gmail.com>
2021-10-02 02:09:35 -0500

    Added Spinner

--- src/pages/NewProduct.js
+++ src/pages/NewProduct.js
@@ -1,5 +1,7 @@
 // import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import Spinner from '../components/common/Spinner';
 import './NewProduct.css';
 
 // import useFetchCategory from '../hooks/useFetch-hook';
@@ -7,6 +9,8 @@ import './NewProduct.css';
 const NewProduct = () => {
   // const results = useFetchCategory('categorias');
   // console.log(results);
+
+  const [loading, setLoading] = useState(false);
 
   const {
     register,
@@ -17,6 +21,7 @@ const NewProduct = () => {
   // const onSubmit = (data) => console.log(data);
 
   const onSubmit = (data) => {
+    setLoading(true);
     fetch('http://localhost:5000/Producto', {
       method: 'POST', // or 'PUT'
       body: JSON.stringify(data), // data can be `string` or {object}!
@@ -25,10 +30,14 @@ const NewProduct = () => {
       },
     })
       .then((res) => res.json())
-      .catch((error) => console.error('Error:', error))
-      .then((response) =>
-        console.log('Se agregó el siguiente producto:', response)
-      );
+      .catch((error) => {
+        console.error('Error:', error);
+        setLoading(false);
+      })
+      .then((response) => {
+        console.log('Se agregó el siguiente producto:', response);
+        setLoading(false);
+      });
   };
 
   // const handleSubmit = (ev) => {
@@ -37,43 +46,59 @@ const NewProduct = () => {
   // };
 
   return (
-    <div className="mx-auto p-4 block w-11/12 md:w-4/6">
-      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-        <div className="mb-4 inline-block w-1/4">
-          <label className="font-bold block md:mb-0" htmlFor="inline-full-name">
-            Marca
-          </label>
-          <input
-            // defaultValue="test"
-            {...register('marca', { required: true })}
-            className="border my-1 px-5 py-3 rounded-xl w-full"
-            placeholder="Marca"
-          />
-          {errors.marca && (
-            <p className="text-red-600 absolute">This field is required</p>
-          )}
-        </div>
+    <div className="w-screen">
+      {loading ? <Spinner /> : null}
+      {/* <Spinner loading={loading} /> */}
+      <div className="mx-auto p-4 block w-11/12 md:w-4/6">
+        <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+          <div className="mb-6 inline-block w-1/3">
+            <label
+              className="font-bold block md:mb-0"
+              htmlFor="inline-full-name"
+            >
+              Marca
+            </label>
+            <input
+              // defaultValue="test"
+              {...register('marca', { required: true })}
+              className="border my-1 px-5 py-3 rounded-xl w-full"
+              placeholder="Marca"
+            />
+            {errors.marca && (
+              <p className="text-red-600 absolute text-sm">
+                This field is required
+              </p>
+            )}
+          </div>
 
-        <div className="mb-4 inline-block pl-4 w-3/4">
-          <label className="font-bold block md:mb-0" htmlFor="inline-full-name">
-            Nombre
-          </label>
-          <input
-            // defaultValue="test"
-            {...register('nombre', { required: true })}
-            className="border my-1 px-5 py-3 rounded-xl w-full"
-            placeholder="Nombre"
-          />
-          {errors.nombre && (
-            <p className="text-red-600 absolute">This field is required</p>
-          )}
-        </div>
+          <div className="mb-4 inline-block pl-4 w-2/3">
+            <label
+              className="font-bold block md:mb-0"
+              htmlFor="inline-full-name"
+            >
+              Nombre
+            </label>
+            <input
+              // defaultValue="test"
+              {...register('nombre', { required: true })}
+              className="border my-1 px-5 py-3 rounded-xl w-full"
+              placeholder="Nombre"
+            />
+            {errors.nombre && (
+              <p className="text-red-600 absolute text-sm">
+                This field is required
+              </p>
+            )}
+          </div>
 
-        <div className="mb-4 inline-block w-auto w-2/4">
-          <label className="font-bold block md:mb-0" htmlFor="inline-full-name">
-            Categoria
-          </label>
-          {/* <input
+          <div className="mb-6 inline-block w-full md:w-2/4">
+            <label
+              className="font-bold block md:mb-0"
+              htmlFor="inline-full-name"
+            >
+              Categoria
+            </label>
+            {/* <input
             {...register('categoria', { required: true })}
             className="border my-1 p-2 rounded-xl w-full"
             placeholder="Categoria"
@@ -82,79 +107,97 @@ const NewProduct = () => {
             <p className="text-red-600">This field is required</p>
           )} */}
 
-          <select
-            {...register('categoria', { pattern: /^(?!.*?categoria).*/ })}
-            className="border my-1 px-5 py-3 rounded-xl w-full"
-          >
-            <option value="Lubricantes, aceites y quimicos">
-              Lubricantes, aceites y quimicos
-            </option>
-            <option value="male">male</option>
-            <option value="other">other</option>
-          </select>
-          {errors.categoria && errors.categoria.type == 'pattern' && (
-            <p className="text-red-600 absolute">Selecciona una categoria</p>
-          )}
-        </div>
-
-        <div className="mb-4 inline-block w-1/4 pl-4">
-          <label className="font-bold block md:mb-0" htmlFor="inline-full-name">
-            Precio
-          </label>
-          <div className="border my-1  rounded-xl w-full inline-flex price">
-            <p className="inline-block pl-5 pr-1">
-              <strong>$</strong>
-            </p>
-            <input
-              {...register('precio', {
+            <select
+              {...register('categoria', {
                 required: true,
-                minLength: 1,
-                pattern: /^[0-9]+\.[0-9][0-9]$/,
+                minLength: 2,
+                pattern: /^(?!.*?Categoría).*/,
+              })}
+              className="border my-1 px-5 py-3 rounded-xl w-full"
+            >
+              <option>Categoría</option>
+              <option value="Lubricantes, aceites y quimicos">
+                Lubricantes, aceites y quimicos
+              </option>
+            </select>
+            {errors.categoria && (
+              <p className="text-red-600 absolute text-sm">
+                Selecciona una categoria
+              </p>
+            )}
+          </div>
+
+          <div className="mb-6 inline-block w-2/4 md:w-1/4 md:pl-4">
+            <label
+              className="font-bold block md:mb-0"
+              htmlFor="inline-full-name"
+            >
+              Precio
+            </label>
+            <div className="border my-1  rounded-xl w-full inline-flex price">
+              <p className="inline-block pl-5 pr-1">
+                <strong>$</strong>
+              </p>
+              <input
+                {...register('precio', {
+                  required: true,
+                  minLength: 1,
+                  pattern: /^[0-9]+\.[0-9][0-9]$/,
+                })}
+                type="number"
+                min="0"
+                step="0.01"
+                className="inline-block pr-5 py-3"
+                placeholder="Precio"
+              />
+            </div>
+            {/* errors will return when field validation fails  */}
+            {errors.precio && errors.precio.type == 'pattern' && (
+              <p className="text-red-600 absolute text-sm">
+                Solo se admiten números
+              </p>
+            )}
+            {errors.precio && (
+              <p className="text-red-600 absolute text-sm">
+                Solo se admiten números
+              </p>
+            )}
+          </div>
+
+          <div className="mb-6 inline-block pl-4 w-2/4 md:w-1/4">
+            <label
+              className="font-bold block md:mb-0"
+              htmlFor="inline-full-name"
+            >
+              Cantidad
+            </label>
+            <input
+              {...register('cantidad', {
+                required: true,
+                pattern: /^[0-9]*$/,
               })}
               type="number"
               min="0"
-              step="0.01"
-              className="inline-block pr-5 py-3"
-              placeholder="Precio"
+              step="1"
+              className="border my-1 px-5 py-3 rounded-xl w-full"
+              placeholder="Cantidad"
             />
+            {/* errors will return when field validation fails  */}
+            {errors.cantidad && (
+              <p className="text-red-600 absolute text-sm">
+                This field is required
+              </p>
+            )}
           </div>
-          {/* errors will return when field validation fails  */}
-          {errors.precio && errors.precio.type == 'pattern' && (
-            <p className="text-red-600 absolute">Solo se admiten números</p>
-          )}
-          {errors.precio && (
-            <p className="text-red-600 absolute">Solo se admiten números</p>
-          )}
-        </div>
 
-        <div className="mb-4 inline-block pl-4 w-1/4">
-          <label className="font-bold block md:mb-0" htmlFor="inline-full-name">
-            Cantidad
-          </label>
-          <input
-            {...register('cantidad', {
-              required: true,
-              pattern: /^[0-9]*$/,
-            })}
-            type="number"
-            min="0"
-            step="1"
-            className="border my-1 px-5 py-3 rounded-xl w-full"
-            placeholder="Cantidad"
-          />
-          {/* errors will return when field validation fails  */}
-          {errors.cantidad && (
-            <p className="text-red-600 absolute">This field is required</p>
-          )}
-        </div>
-
-        <button
-          type="submit"
-          className="send-button py-2 px-5 py-3 mt-2 rounded-xl text-white font-medium block w-1/4 "
-        >
-          Enviar
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="send-button py-2 px-5 py-3 mt-2 rounded-xl text-white font-medium block w-full md:w-1/4 focus:outline-none"
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
